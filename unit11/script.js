dfa0e07d6b70dac3916f0c0f4fc198f7039ddb19
JHJFE <ghwo9611@gmail.com>
2023-01-09 22:14:11 +0900

    2023-01-09 더미 데이터 출력

---
+++ unit11/script.js
@@ -0,0 +1,67 @@
+
+// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
+console.log(agoraStatesDiscussions);
+
+// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+const convertToDiscussion = (obj) => {
+  const li = document.createElement("li"); // li 요소 생성
+  li.className = "discussion__container"; // 클래스 이름 지정
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[0].author;
+  avatarWrapper.append(avatarImg);
+
+
+  const title = document.createElement('h2');
+  const a = document.createElement('a');
+
+  a.textContent = obj.title;
+  a.href = obj.url;
+
+  title.append(a);
+  discussionContent.append(title);
+
+  const time = Date(obj.createAt);
+
+  const information = document.createElement('div');
+  information.classList.add('discussion__information');
+
+  information.textContent = `${obj.author} / ${time.toLocaleLowerCase()}`;
+  title.append(information);
+
+
+  const check = document.createElement('div');
+  check.classList.add('discussion__answered')
+
+  if (obj.answer !== null) {
+    check.textContent = '☑';
+  }
+ 
+
+
+
+  li.append(avatarWrapper, discussionContent, discussionAnswered,check);
+  return li;
+};
+
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+const render = (element) => {
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  return;
+};
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+const ul = document.querySelector("ul.discussions__container");
+
+render(ul);
